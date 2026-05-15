4a00a94e55f787abd83b815100e89f5e81582e3c
Umair khan <ukkhan5476@gmail.com>
2026-05-15 20:34:57 +0530

    feat(auth-service): implement database connection and update config handling

---
+++ services/auth-service/src/config/db.config.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import CONFIG from "./config.js";
+const dbConnect = async (): Promise<void> => {
+  try {
+    if (!CONFIG.MONGO_URI) {
+      console.log("Mongo uri  not ound");
+      process.exit(1);
+    }
+    await mongoose.connect(CONFIG.MONGO_URI);
+    console.log("Auth service db connected successfully")
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("Auth service Db Connection failed", error.message);
+      process.exit(1);
+    } else {
+      console.log(
+        "Auth service Db connection failed with some other Error",
+        error,
+      );
+      process.exit(1);
+    }
+  }
+};
+
+export default dbConnect;
--- services/auth-service/src/server.ts
+++ services/auth-service/src/server.ts
@@ -1,5 +1,8 @@
 import app from "./app.js";
 import CONFIG from "./config/config.js";
+import dbConnect from "./config/db.config.js";
+
+dbConnect();
 
 app.listen(CONFIG.PORT, () => {
   console.log(`Auth Server is runing at ${CONFIG.PORT}`);
