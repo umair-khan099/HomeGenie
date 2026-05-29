036c89bb0a80d1e4a3601d7098f1dc818df9ea93
Umair khan <ukkhan5476@gmail.com>
2026-05-30 04:11:31 +0530

    feat(auth-service): add error handling and validation middleware for improved request processing

---
+++ services/auth-service/src/middlewares/error.middleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+
+import { AppError } from "../utils/appError.js";
+
+export const errorHandler = (
+  err: AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const statusCode = err.statusCode || 500;
+
+  return res.status(statusCode).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+    errors: err.errors || [],
+  });
+};
---
+++ services/auth-service/src/middlewares/validate.middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import { ZodSchema } from "zod";
+
+import { AppError } from "../utils/appError.js";
+
+export const validate =
+  (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+
+    if (!result.success) {
+      const formattedErrors = result.error.issues.map((err) => ({
+        field: err.path.join("."),
+        message: err.message,
+      }));
+
+      return next(new AppError(400, "Validation Failed", formattedErrors));
+    }
+
+    req.body = result.data;
+
+    next();
+  };
