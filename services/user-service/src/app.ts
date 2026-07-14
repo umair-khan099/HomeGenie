4ddedbc0672ac91be62ca9caa4e1b47d7a93c3eb
Umair khan <ukkhan5476@gmail.com>
2026-07-14 16:44:51 +0530

    feat(user-service): add Cloudinary integration for file uploads and configure express-fileupload

--- services/user-service/src/app.ts
+++ services/user-service/src/app.ts
@@ -1,11 +1,18 @@
 import express, { Application } from "express";
 import dotenv from "dotenv";
 import { profileRouter } from "./routes/profile.route.js";
+import fileUpload from "express-fileupload";
 
 const app: Application = express();
 
 dotenv.config();
 
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  }),
+);
 app.use(express.json());
 app.use("/", profileRouter);
 
---
+++ services/user-service/src/utils/cloudUpload.ts
@@ -0,0 +1,36 @@
+import v2 from "../config//cloudinary.config.js";
+import { UploadApiOptions } from "cloudinary";
+
+interface OptionsI {
+  resource_type: string;
+  folder: string;
+  height?: any;
+  width?: any;
+}
+
+export const fileUpload = async (
+  file: any,
+  folder: string,
+  height: any,
+  width: any,
+) => {
+  try {
+    const options: UploadApiOptions = {
+      resource_type: "auto",
+      folder: folder,
+    };
+    if (height) {
+      options.height = height;
+    }
+    if (width) {
+      options.width = width;
+    }
+    if (!file.tempFilePath) {
+      throw new Error("File path not found");
+    }
+
+    return await v2.uploader.upload(file.tempFilePath, options);
+  } catch (error) {
+    console.log("Cloudinary file uploade error", error);
+  }
+};
